f554ccb8ef8f3063bd45082fd85ee0720e8205de
ahmed86365 <44976294+ahmed86365@users.noreply.github.com>
2018-11-20 09:23:53 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "1"
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '424892118574563328') return;
+
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/OvErDz1");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
